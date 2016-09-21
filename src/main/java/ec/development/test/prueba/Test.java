37f456bb9c25a4e919002094064c5a3531c95d8b
Christian Mena <christian.91.gm@hotmail.com>
2016-09-20 23:00:39 -0500

    sad

--- src/main/java/ec/development/test/prueba/Test.java
+++ src/main/java/ec/development/test/prueba/Test.java
@@ -16,7 +16,7 @@ public class Test {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Juanito me la chupas");
+        System.out.println("No vale la huevada");
     }
     
 }
